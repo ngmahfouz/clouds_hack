c5098924bd141b1bca9f2e938149ed33d2594b66
James Aymard Assiene Moudie <jassiene@beluga3.int.ets1.calculquebec.ca>
2020-03-06 10:31:21 -0500

    Added code for parallel runs

--- data_scratch.py
+++ data_scratch.py
@@ -15,23 +15,39 @@ import torch.nn as nn
 import train
 from torchvision.utils import save_image
 from res_discriminator import MultiDiscriminator, Discriminator
+import argparse
+import yaml
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-o", "--output_dir", type=str, help="Where to save files", default=".")
+parser.add_argument("-c", "--config_file", type=str, help="YAML configuration file", default="default_training_config.yaml")
+
+args = parser.parse_args()
 
 importlib.reload(data)
 importlib.reload(model)
 importlib.reload(train)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-opts = Dict({"lr": 1e-4, "n_epoch": 10001, "save_every" : 500, "noise_dim" : 2, "disc_step" : 1})
+#opts = Dict({"lr": 1e-4, "n_epochs": 10001, "save_every" : 500, "noise_dim" : 2, "disc_step" : 1})
+
+with open(args.config_file) as f:
+    yaml_config = yaml.load(f, Loader=yaml.FullLoader)
+    data_args = yaml_config['data']
+    model_args = yaml_config["model"]
+    val_args = yaml_config["val"]
+    train_args = yaml_config["train"]
 
 clouds = data.LowClouds("/scratch/sankarak/data/low_clouds/", 10)
 loader = DataLoader(clouds, batch_size=10)
-dec = model.Deconvolver(8, 128, opts["noise_dim"])
+dec = model.Deconvolver(8, 128, model_args["noise_dim"])
 dec = dec.to(device)
 
 disc = MultiDiscriminator(in_channels=1, device=device).to(device)
 models = Dict({"g": dec, "d": disc})
 optimizers = Dict({
-    "g": torch.optim.Adam(dec.parameters(), lr=opts["lr"]),
-    "d": torch.optim.Adam(disc.parameters(), lr= 0.01 * opts["lr"])
+    "g": torch.optim.Adam(dec.parameters(), lr=train_args["lr_g"]),
+    "d": torch.optim.Adam(disc.parameters(), lr=train_args["lr_d"])
 })
 
 def infer(generator, x, noise_dim, device):
@@ -46,23 +62,23 @@ def save_images(dec, loader, i, n_infer=5):
         y_mean = y.mean(0)
         print("Loss of the mean image : ", ((y_mean - y) ** 2).mean())
 
-        save_image(y, f"original_imgs_{i}.png")
-        save_image(y_mean, f"mean_imgs_{i}.png")
+        save_image(y, f"{args.output_dir}/original_imgs_{i}.png")
+        save_image(y_mean, f"{args.output_dir}/mean_imgs_{i}.png")
         y_hats = []
         for j in range(n_infer):
-            y_hats.append(infer(dec, x, opts.noise_dim, device))
+            y_hats.append(infer(dec, x, model_args["noise_dim"], device))
 
         y_hats = torch.cat(y_hats, axis=0)
-        save_image(y_hats, f"predicted_imgs_{i}-{j}.png")
+        save_image(y_hats, f"{args.output_dir}/predicted_imgs_{i}-{j}.png")
 
 
-for i in range(opts.n_epoch):
+for i in range(train_args["n_epochs"]):
     models, avg_loss = train.train(models, loader, optimizers, nn.MSELoss(), device)
-    if i % opts.save_every == 0:
-        save_images(dec, loader, i)
+    if i % train_args["save_every_epochs"] == 0:
+        save_images(dec, loader, i, train_args["n_infer"])
     print(f"Discriminator loss : {avg_loss.d} - Generator loss : {avg_loss.g} - Matching loss: {avg_loss.matching}")
 
-torch.save(dec.state_dict(), "dec_test.pth")
+torch.save(dec.state_dict(), f"{args.output_dir}/dec_test.pth")
 
 
 # go through loader and look at y_hat
---
+++ parallel_run.py
@@ -0,0 +1,80 @@
+import argparse
+import yaml
+import pdb
+import os
+import numpy as np
+from textwrap import dedent
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-c", "--config_file", type=str, help="YAML configuration file", default="default.yaml")
+parser.add_argument("-d", "--default_config_file", type=str, help="YAML configuration file for the default training", default="default_training_config.yaml")
+
+args = parser.parse_args()
+
+
+# Open the "outer" configuration file 
+with open(args.config_file) as f:
+    yaml_config = yaml.load(f, Loader=yaml.FullLoader)
+    # pdb.set_trace()
+    sbatch_args = yaml_config['runs'][0]['sbatch']
+    experiments_args = yaml_config['experiment']
+    data_args = yaml_config['runs'][0]['config']['data']
+
+# Open the default "inner" configuration file (containing default parameters for the model itself, its training and its validation)
+# These default values will be replaced by the ones from the "outer" configuration file (using sampling for example)
+with open(args.default_config_file) as f:
+    default_training_yaml_config = yaml.load(f, Loader=yaml.FullLoader)
+    # pdb.set_trace()
+    sbatch_args = yaml_config['runs'][0]['sbatch']
+    experiments_args = yaml_config['experiment']
+    data_args = yaml_config['runs'][0]['config']['data']
+
+
+#Go through each "module" (can be model or train) parameters. Sample a value of the parameter if necessary and replace the default "inner" config file value with this one
+for i in range(experiments_args['repeat']):
+    current_training_config = default_training_yaml_config #defaut config
+    tunable_modules = yaml_config['runs'][0]['config'] # possible values for the hyper-parameters
+    for tunable_module_name in tunable_modules:
+        tunable_module = tunable_modules[tunable_module_name]
+        current_run_args = default_training_yaml_config[tunable_module_name] # by default, this run takes the default config
+        for hyper_parameter in tunable_module:
+            current_run_args[hyper_parameter] = tunable_module[hyper_parameter]
+            sample_method = isinstance(tunable_module[hyper_parameter], dict) and tunable_module[hyper_parameter].get("sample", None)
+            if sample_method is not None:
+                if sample_method == "list":
+                    current_run_args[hyper_parameter] = np.random.choice(tunable_module[hyper_parameter]["from"]).item()
+        
+        current_training_config[tunable_module_name] = current_run_args
+
+    current_run_path = f"{experiments_args['exp_dir']}/{experiments_args['name']}/run_{i}"
+    
+
+    # Define the slurm file
+    slurm_file_template =  dedent(
+    f"""\
+    #!/bin/bash
+    #SBATCH --account=rpp-bengioy            # Yoshua pays for your job
+    #SBATCH --cpus-per-task={sbatch_args['cpu']}                # Ask for 6 CPUs
+    #SBATCH --gres={sbatch_args['gpu']}                     # Ask for 1 GPU
+    #SBATCH --mem={sbatch_args['mem']}                        # Ask for 32 GB of RAM
+    #SBATCH --time={sbatch_args['runtime']}                   # The job will run for 3 hours
+    #SBATCH -o {current_run_path}/slurm-%j.out  # Write the log in $SCRATCH
+    
+    mkdir -p {current_run_path}
+    module load singularity/3.5
+    cd $HOME/clouds_hack/
+    singularity exec --nv --bind $SLURM_TMPDIR,{data_args['path']},{current_run_path} {experiments_args['img_path']} \
+    python3 data_scratch.py -o $SLURM_TMPDIR -c {current_run_path}/training_config.yaml
+    cp $SLURM_TMPDIR/* {current_run_path}""")
+
+    os.makedirs(current_run_path, exist_ok=True)
+    #Write the slurm file
+    with open(f"{current_run_path}/slurm_job.slrm", "w") as f:
+        f.write(slurm_file_template)
+    
+    # Write the training configuration file
+    with open(f"{current_run_path}/training_config.yaml", "w") as f:
+        yaml.dump(current_training_config, f)
+
+    os.system(f"sbatch {current_run_path}/slurm_job.slrm")
