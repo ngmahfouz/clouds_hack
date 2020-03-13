be0e17ee84721b65f22bac8a67cafc33cc6ee7fd
James Aymard Assiene Moudie <jassiene@beluga2.int.ets1.calculquebec.ca>
2020-03-13 14:04:07 -0400

    Added std of mean image and updated configuration files

--- data_scratch.py
+++ data_scratch.py
@@ -40,7 +40,7 @@ with open(args.config_file) as f:
     train_args = yaml_config["train"]
 
 dataset_transforms = get_transforms(data_args)
-clouds = data.LowClouds(data_args["path"], -1, transform=dataset_transforms)
+clouds = data.LowClouds(data_args["path"], data_args["load_limit"], transform=dataset_transforms)
 nb_images = len(clouds)
 train_clouds, val_clouds = torch.utils.data.random_split(clouds, [nb_images - val_args["set_size"], val_args["set_size"]])
 train_loader = DataLoader(train_clouds, batch_size=train_args["batch_size"])
@@ -65,7 +65,8 @@ def save_images(dec, loader, i, n_infer=5):
         x = sample["metos"].to(device)
         y = sample["real_imgs"].to(device)
         y_mean = y.mean(0)
-        print("Loss of the mean image : ", ((y_mean - y) ** 2).mean())
+        print("Loss of the mean image : ", ((y_mean - y) ** 2).mean(), flush=True)
+        print("Standard deviation of the mean image : ", y_mean.std(), flush=True)
 
         save_image(y, f"{args.output_dir}/original_imgs_{i}.png")
         save_image(y_mean, f"{args.output_dir}/mean_imgs_{i}.png")
@@ -74,6 +75,7 @@ def save_images(dec, loader, i, n_infer=5):
             y_hats.append(infer(dec, x, model_args["noise_dim"], device))
 
         y_hats = torch.cat(y_hats, axis=0)
+        print("Standard deviation of the first image : ", y_hats[0].std(), flush=True)
         save_image(y_hats, f"{args.output_dir}/predicted_imgs_{i}-{j}.png")
 
 
@@ -81,7 +83,7 @@ for i in range(train_args["n_epochs"]):
     models, avg_loss = train.train(models, train_loader, optimizers, nn.MSELoss(), device)
     if i % train_args["save_every_epochs"] == 0:
         save_images(dec, val_loader, i, train_args["n_infer"])
-    print(f"Discriminator loss : {avg_loss.d} - Generator loss : {avg_loss.g} - Matching loss: {avg_loss.matching}")
+    print(f"Discriminator loss : {avg_loss.d} - Generator loss : {avg_loss.g} - Matching loss: {avg_loss.matching}", flush=True)
 
 torch.save(dec.state_dict(), f"{args.output_dir}/dec_test.pth")
 
