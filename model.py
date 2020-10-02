98d58219e00fc34bd82ac0a904f6aeff75d6b16d
James Aymard Assiene Moudie <jassiene@beluga3.int.ets1.calculquebec.ca>
2020-10-01 21:13:25 -0400

    Moved the Grayscale2RGB into a separate class/transform to speedup the code. Changed the code style/formatting

--- model.py
+++ model.py
@@ -14,20 +14,29 @@ from torchvision import models, transforms
 import utils
 import torch.nn.functional as F
 
+
 class Deconvolver(nn.Module):
     def __init__(self, n_in, n_out, n_blocks=4, depth_increase_factor=2, noise_dim=2):
         super().__init__()
         self.n_out = n_out
         self.n_in = n_in
-        self.total_n_in = n_in + noise_dim #takes into account both the metos (n_in=8) and the noise (noise_dim) by concatenating their respective vectors
+        self.total_n_in = (
+            n_in + noise_dim
+        )  # takes into account both the metos (n_in=8) and the noise (noise_dim) by concatenating their respective vectors
 
-        self.initial_linear = nn.Linear(self.total_n_in, self.total_n_in * n_out ** 2) # we want to generate images of size height=n_out x width=n_out
+        self.initial_linear = nn.Linear(
+            self.total_n_in, self.total_n_in * n_out ** 2
+        )  # we want to generate images of size height=n_out x width=n_out
         self.metos_batch_norm = nn.BatchNorm1d(n_in)
         cblocks_list = []
         unet_input_channels = self.total_n_in
         for i in range(n_blocks):
-            cblocks_list.append(UNetModule(unet_input_channels, depth_increase_factor * unet_input_channels))
-            unet_input_channels*= 2
+            cblocks_list.append(
+                UNetModule(
+                    unet_input_channels, depth_increase_factor * unet_input_channels
+                )
+            )
+            unet_input_channels *= 2
         self.conv_final = nn.Conv2d(unet_input_channels, 1, kernel_size=1)
         self.model = nn.ModuleList(cblocks_list + [self.conv_final])
 
@@ -45,6 +54,7 @@ class UNetModule(nn.Module):
     """
     One of the "triple layer" blocks in https://arxiv.org/pdf/1505.04597.pdf
     """
+
     def __init__(self, n_in, n_out):
         super().__init__()
         self.conv1 = nn.Conv2d(n_in, n_out, 3, padding=1)
@@ -54,29 +64,41 @@ class UNetModule(nn.Module):
 
     def forward(self, x):
         layers = nn.Sequential(
-            self.conv1, self.bn, self.activation,
-            self.conv2, self.bn, self.activation
+            self.conv1, self.bn, self.activation, self.conv2, self.bn, self.activation
         )
         return layers(x)
 
+
 class DCGANDiscriminator(nn.Module):
-    def __init__(self, img_size=128, ndf=64, nc=1, use_spectral_norm=True, discrete_latent_dim=10, continuous_latent_dim=1, predict_metos=True):
+    def __init__(
+        self,
+        img_size=128,
+        ndf=64,
+        nc=1,
+        use_spectral_norm=True,
+        discrete_latent_dim=10,
+        continuous_latent_dim=1,
+        predict_metos=True,
+    ):
         super().__init__()
 
-        self.log_first_out_channels = int(math.log2(img_size)) - 3 # For 64=2^6 the first output is 8 = 2^3 . For 128 = 2^7 it's 16=2^4 . We remove 3 each time
+        self.log_first_out_channels = (
+            int(math.log2(img_size)) - 3
+        )  # For 64=2^6 the first output is 8 = 2^3 . For 128 = 2^7 it's 16=2^4 . We remove 3 each time
         cblocks_list = []
         self.criterion = nn.BCELoss()
         self.discriminator_head = True
-        
+
         stride = 2
         padding = 1
         prev_out_channels = nc
-        
-        
+
         for i in range(self.log_first_out_channels + 1):
             current_out_channels = ndf * 2 ** i
 
-            conv_layer = nn.Conv2d(prev_out_channels, current_out_channels, 4, stride, padding, bias=False)
+            conv_layer = nn.Conv2d(
+                prev_out_channels, current_out_channels, 4, stride, padding, bias=False
+            )
             self.weights_init(conv_layer)
 
             if use_spectral_norm:
@@ -87,11 +109,16 @@ class DCGANDiscriminator(nn.Module):
             bn = nn.BatchNorm2d(current_out_channels)
             self.weights_init(bn)
             cblocks_list.append(bn)
-            
+
             cblocks_list.append(nn.LeakyReLU(0.2, inplace=True))
             prev_out_channels = current_out_channels
 
-        self.init_q_head(current_out_channels, discrete_latent_dim, continuous_latent_dim, predict_metos)
+        self.init_q_head(
+            current_out_channels,
+            discrete_latent_dim,
+            continuous_latent_dim,
+            predict_metos,
+        )
 
         conv_layer = nn.Conv2d(current_out_channels, 1, 4, 1, 0, bias=False)
         self.weights_init(conv_layer)
@@ -102,14 +129,22 @@ class DCGANDiscriminator(nn.Module):
         self.model = nn.ModuleList(cblocks_list)
         self.disc_head = nn.Sequential(last_conv, nn.Sigmoid())
 
-    def init_q_head(self, feature_maps_depth=512, discrete_latent_dim=10, continuous_latent_dim=1, predict_metos=True):
+    def init_q_head(
+        self,
+        feature_maps_depth=512,
+        discrete_latent_dim=10,
+        continuous_latent_dim=1,
+        predict_metos=True,
+    ):
         i = feature_maps_depth
         cblocks_list = []
         stride = 1
         padding = 0
         while i != 256:
             current_out_channels = int(i / 2)
-            conv_layer = nn.Conv2d(i, current_out_channels, 1, stride, padding, bias=False)
+            conv_layer = nn.Conv2d(
+                i, current_out_channels, 1, stride, padding, bias=False
+            )
             self.weights_init(conv_layer)
             cblocks_list.append(conv_layer)
 
@@ -118,25 +153,33 @@ class DCGANDiscriminator(nn.Module):
             cblocks_list.append(bn)
 
             i = current_out_channels
-        
+
         self.q_conv_blocks = nn.ModuleList(cblocks_list)
 
         self.q_conv1 = nn.Conv2d(current_out_channels, 128, 4, 1, 0, bias=False)
 
-        self.q_conv_disc = nn.Conv2d(128, discrete_latent_dim, 1) if discrete_latent_dim != 0 else None
-        
-        self.q_conv_mu = nn.Conv2d(128, continuous_latent_dim + predict_metos * 8, 1) if (continuous_latent_dim + predict_metos) != 0 else None
-        self.q_conv_var = nn.Conv2d(128, continuous_latent_dim, 1) if continuous_latent_dim != 0 else None
+        self.q_conv_disc = (
+            nn.Conv2d(128, discrete_latent_dim, 1) if discrete_latent_dim != 0 else None
+        )
 
+        self.q_conv_mu = (
+            nn.Conv2d(128, continuous_latent_dim + predict_metos * 8, 1)
+            if (continuous_latent_dim + predict_metos) != 0
+            else None
+        )
+        self.q_conv_var = (
+            nn.Conv2d(128, continuous_latent_dim, 1)
+            if continuous_latent_dim != 0
+            else None
+        )
 
     def weights_init(self, m):
         classname = m.__class__.__name__
-        if classname.find('Conv') != -1:
+        if classname.find("Conv") != -1:
             m.weight.data.normal_(0.0, 0.02)
-        elif classname.find('BatchNorm') != -1:
+        elif classname.find("BatchNorm") != -1:
             m.weight.data.normal_(1.0, 0.02)
             m.bias.data.fill_(0)
-
 
     def compute_loss(self, x, gt):
         """Computes the BCELoss between model output and scalar gt"""
@@ -152,14 +195,20 @@ class DCGANDiscriminator(nn.Module):
 
         x = F.leaky_relu(self.q_conv1(x), 0.1, inplace=True)
 
-        disc_logits = self.q_conv_disc(x).squeeze() if self.q_conv_disc is not None else None
+        disc_logits = (
+            self.q_conv_disc(x).squeeze() if self.q_conv_disc is not None else None
+        )
 
         # Not used during training for celeba dataset.
         mu = self.q_conv_mu(x).squeeze() if self.q_conv_mu is not None else None
-        var = torch.exp(self.q_conv_var(x).squeeze()) if self.q_conv_var is not None else None
+        var = (
+            torch.exp(self.q_conv_var(x).squeeze())
+            if self.q_conv_var is not None
+            else None
+        )
 
         return disc_logits, mu, var
-    
+
     def forward(self, input):
         output = input
         for layer in self.model:
@@ -173,34 +222,45 @@ class DCGANDiscriminator(nn.Module):
             return self.qhead_forward(output)
 
 
-
 class DCGANGenerator(nn.Module):
     def __init__(self, img_size, layers_to_film=[], nz=100, ngf=64, nc=1):
         super().__init__()
-        self.log_first_out_channels = int(math.log2(img_size)) - 3 # For 64=2^6 the first output is 8 = 2^3 . For 128 = 2^7 it's 16=2^4 . We remove 3 each time
+        self.log_first_out_channels = (
+            int(math.log2(img_size)) - 3
+        )  # For 64=2^6 the first output is 8 = 2^3 . For 128 = 2^7 it's 16=2^4 . We remove 3 each time
         self.layers_to_film = layers_to_film
         cblocks_list = []
-        
+
         stride = 1
         padding = 0
         prev_out_channels = nz
-        
-        
+
         for i in range(self.log_first_out_channels, -1, -1):
             current_out_channels = ngf * 2 ** i
 
-            cblocks_list.append(nn.ConvTranspose2d(prev_out_channels, current_out_channels, 4, stride, padding, bias=False))
+            cblocks_list.append(
+                nn.ConvTranspose2d(
+                    prev_out_channels,
+                    current_out_channels,
+                    4,
+                    stride,
+                    padding,
+                    bias=False,
+                )
+            )
             cblocks_list.append(nn.BatchNorm2d(current_out_channels))
-            if (self.log_first_out_channels - i) in self.layers_to_film: # Index of the layer to film. If we want to FiLM the first layer (index 0), it correspond to i = self.log_first_out_channels
+            if (
+                self.log_first_out_channels - i
+            ) in self.layers_to_film:  # Index of the layer to film. If we want to FiLM the first layer (index 0), it correspond to i = self.log_first_out_channels
                 cblocks_list.append(FiLM(output_dim=current_out_channels))
             cblocks_list.append(nn.ReLU(True))
             stride = 2
             padding = 1
             prev_out_channels = current_out_channels
 
-        self.model = nn.ModuleList(cblocks_list + [nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False), nn.Tanh()])
-
-        
+        self.model = nn.ModuleList(
+            cblocks_list + [nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False), nn.Tanh()]
+        )
 
     def forward(self, metos, noise):
         x_ = noise.unsqueeze(-1).unsqueeze(-1)
@@ -211,44 +271,51 @@ class DCGANGenerator(nn.Module):
                 x_ = layer(x_)
         return x_
 
-class MetosRegressor(nn.Module):
 
+class MetosRegressor(nn.Module):
     def __init__(self, train_args, device):
         super().__init__()
         self.device = device
-        feature_extractor, feature_extractor_input_size, self.num_features = utils.initialize_model(train_args["feature_extractor_model"], feature_extract=train_args["freeze_ft"], use_pretrained=train_args["use_pretrained_ft"], freeze_first_n=train_args["freeze_first_n"])
-        feature_extractor = feature_extractor.to(device)
-        feature_extractor_transforms = transforms.Compose([transforms.ToPILImage(),
-                                    transforms.Resize((feature_extractor_input_size, feature_extractor_input_size)),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                    std=[0.229, 0.224, 0.225])])
-
-        self.feature_extraction = {"extractor" : feature_extractor, "transformations": feature_extractor_transforms}
+        (
+            feature_extractor,
+            feature_extractor_input_size,
+            self.num_features,
+        ) = utils.initialize_model(
+            train_args["feature_extractor_model"],
+            feature_extract=train_args["freeze_ft"],
+            use_pretrained=train_args["use_pretrained_ft"],
+            freeze_first_n=train_args["freeze_first_n"],
+        )
+        self.feature_extractor = feature_extractor
 
         self.regression_head = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=self.num_features, out_features=train_args["hidden_features"]),
+            nn.Linear(
+                in_features=self.num_features,
+                out_features=train_args["hidden_features"],
+            ),
             nn.BatchNorm1d(train_args["hidden_features"]),
             nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(in_features=train_args["hidden_features"], out_features=train_args["hidden_features"]),
+            nn.Dropout(0.4),
+            nn.Linear(
+                in_features=train_args["hidden_features"],
+                out_features=train_args["hidden_features"],
+            ),
             nn.BatchNorm1d(train_args["hidden_features"]),
             nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(in_features=train_args["hidden_features"], out_features=train_args["num_metos"])
+            nn.Dropout(0.4),
+            nn.Linear(
+                in_features=train_args["hidden_features"],
+                out_features=train_args["num_metos"],
+            ),
         )
 
     def forward(self, x):
-        
-        rgb_x = torch.cat([x] * 3, dim=1) #converts grayscale to RGB by replicating the single channel 3 times
-        rgb_normalized_x = []
-        for j in range(x.shape[0]):
-            rgb_normalized_x.append(self.feature_extraction["transformations"](rgb_x[j].cpu()))
-        rgb_x = torch.stack(rgb_normalized_x).to(self.device)
-        feature_maps_x = self.feature_extraction["extractor"](rgb_x)
+
+        feature_maps_x = self.feature_extractor(x)
 
         return self.regression_head(feature_maps_x)
+
 
 # from https://github.com/christiancosgrove/pytorch-spectral-normalization-gan
 # /blob/master/spectral_normalization.py
